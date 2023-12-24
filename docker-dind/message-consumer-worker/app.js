a5522e2cc1bfe1d6531c96343cfc27a1948d8112
Edvan Ronchi <edvan_ronchi@hotmail.com>
2023-12-24 15:31:45 -0300

    #:feat: Added an environment variable to receive the Fibonacci sequence.

--- docker-dind/message-consumer-worker/app.js
+++ docker-dind/message-consumer-worker/app.js
@@ -1,38 +1,50 @@
 const amqp = require('amqplib');
+const process = require('process');
 
-function fibonacci(n) {
+const fibonacci = process.env.FIBONACCI || 40;
+
+function processFibonacci(n) {
     if (n <= 1) {
         return n;
     } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        return processFibonacci(n - 1) + processFibonacci(n - 2);
     }
 }
 
 async function consumer() {
-    try {
-        const connection = await amqp.connect('amqp://guest:guest@172.17.0.1:5672');
-        const channel = await connection.createChannel();
+    const urlDockerDind = 'amqp://guest:guest@172.17.0.1:5672';
 
-        const exchangeName = 'ex-message';
-        const queueName = 'queue-message';
+    const connection = await amqp.connect(urlDockerDind);
+    const channel = await connection.createChannel();
 
-        await channel.assertExchange(exchangeName, 'direct', { durable: true });
+    const exchangeName = 'ex-message';
+    const queueName = 'queue-message';
 
-        await channel.assertQueue(queueName, { durable: true });
+    await channel.assertExchange(exchangeName, 'direct', { durable: true });
 
-        await channel.bindQueue(queueName, exchangeName, '');
+    await channel.assertQueue(queueName, { durable: true });
 
-        console.log(`Aguardando mensagens da exchange ${exchangeName} na fila ${queueName}`);
+    await channel.bindQueue(queueName, exchangeName, '');
 
-        channel.consume(queueName, (msg) => {
-            if (msg.content) {
-                console.log(fibonacci(1));
-                console.log(`Mensagem recebida: ${msg.content.toString()}`);
-            }
-        }, { noAck: true });
-    } catch (error) {
-        console.error(error);
-    }
+    await channel.prefetch(1);
+
+    console.log(`Waiting for messages from the exchange ${exchangeName} in the queue ${queueName}`);
+
+    channel.consume(queueName, (msg) => {
+        if (!msg.content) {
+           return;
+        }
+
+        const start = performance.now();
+        console.log(`Message received: ${msg.content.toString()}`);
+        console.log('Fibonaci: ' + fibonacci);
+        console.log('Result: ' + processFibonacci(fibonacci));
+        const end = performance.now();
+
+        console.log('Time to process: ' + (end - start) + 'ms');
+
+        channel.ack(msg);
+    }, { noAck: false });
 }
 
 consumer();
